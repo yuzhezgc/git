d1b803a7e4c330b4b7fe87519dc07cfd2aa75474
Zhang <zhang@atguigu.com>
2021-02-19 18:55:36 +0800

    fix first commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
+        System.out.println("abc");
+        System.out.println("adc");
     }
 }
